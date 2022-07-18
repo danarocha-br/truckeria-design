e5e93f1aa5ba2de32730a9d97616b77797c788f3
Dana Rocha <hello@danarocha.com>
2022-07-18 18:46:53 +0300

    :sparkles: (Logo): component added to lib

---
+++ src/components/Logo/Logo.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Logo, LogoProps } from '.';
+
+export default {
+  title: 'Components/Logo',
+  component: Logo,
+  parameters: {
+    layout: 'centered',
+    status: 'stable', // beta| deprecated | review | stable,
+    design: {
+      type: 'figma',
+      url: '',
+    },
+  },
+  args: {},
+} as Meta;
+
+const Template: Story<LogoProps> = args => <Logo {...args} />;
+
+export const Default = Template.bind({});
+export const Symbol = Template.bind({});
+Symbol.args = {
+  variant: 'symbol',
+};
---
+++ src/components/Logo/styles.ts
@@ -0,0 +1,27 @@
+import { styled, darkTheme } from '../../stitches.config';
+
+export const MainColor = styled('path', {
+  fill: 'hsla(217, 13%, 19%, 1)',
+
+  [`.${darkTheme} &`]: {
+    fill: 'hsla(37, 42%, 65%, 1)',
+  },
+});
+
+export const BrandColor = styled('path', {
+  fill: 'hsla(10, 100%, 66%, 0.94)',
+});
+
+export const Svg = styled('svg', {
+  variants: {
+    size: {
+      sm: {},
+      md: {
+        transform: 'scale(1.3)',
+      },
+    },
+  },
+  variantsDefault: {
+    size: 'sm',
+  },
+});
