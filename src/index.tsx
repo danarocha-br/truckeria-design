d9b067d10078b77ca9b447dcd43e297efa54e8b2
Dana Rocha <hello@danarocha.com>
2022-07-22 18:34:15 +0300

    :arrow_up: (Upgrade package to have the latest exports):

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ export * from './components/Text';
 export * from './components/Icon';
 
 export * from './components/Button';
+export * from './components/Callout';
 export * from './components/FormControl';
 export * from './components/FormError';
 export * from './components/Link';
 export * from './components/Logo';
 export * from './components/Spinner';
+export * from './components/Stepper';
 export * from './components/TextInput';
 export * from './components/Toast';
 
