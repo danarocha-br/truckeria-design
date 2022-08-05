7d0e25986b892bd048ca91235b7687d8e03fcf50
Dana Rocha <hello@danarocha.com>
2022-08-05 14:38:55 +0300

    :sparkles: (ScrollArea): prop to hide scroll

--- src/components/ScrollArea/index.tsx
+++ src/components/ScrollArea/index.tsx
@@ -1,24 +1,37 @@
 import { CSS } from '@stitches/react';
 import React from 'react';
+import { ScrollAreaProps } from '@radix-ui/react-scroll-area';
 
 import * as S from './styles';
 import { config } from '../../stitches.config';
 
-export type ScrollAreaProps = {
+export type CustomScrollAreaProps = {
   children: React.ReactNode;
   css?: CSS<typeof config>;
-};
+  hideScrollHorizontal?: boolean;
+  hideScrollVertical?: boolean;
+} & ScrollAreaProps;
 
-export const ScrollArea = ({ children, css }: ScrollAreaProps) => (
-  <S.ScrollAreaRoot>
+export const ScrollArea = ({
+  children,
+  css,
+  hideScrollHorizontal = false,
+  hideScrollVertical = false,
+  ...props
+}: CustomScrollAreaProps) => (
+  <S.ScrollAreaRoot {...props}>
     <S.ScrollAreaViewport css={css}>{children}</S.ScrollAreaViewport>
 
-    <S.Scrollbar orientation="vertical">
-      <S.ScrollAreaThumb />
-    </S.Scrollbar>
-    <S.Scrollbar orientation="horizontal">
-      <S.ScrollAreaThumb />
-    </S.Scrollbar>
+    {!hideScrollVertical && (
+      <S.Scrollbar orientation="vertical">
+        <S.ScrollAreaThumb />
+      </S.Scrollbar>
+    )}
+    {!hideScrollHorizontal && (
+      <S.Scrollbar orientation="horizontal">
+        <S.ScrollAreaThumb />
+      </S.Scrollbar>
+    )}
     <S.ScrollAreaCorner />
   </S.ScrollAreaRoot>
 );
